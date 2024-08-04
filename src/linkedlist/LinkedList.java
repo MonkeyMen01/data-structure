7ba7865e170bc6fd21bef760a2eb6afd07eae8ff
Anddie Chen <d0981843347@gmail.com>
2024-08-04 18:22:03 +0800

    refactor name

---
+++ src/linkedlist/LinkedList.java
@@ -0,0 +1,16 @@
+package linkedlist;
+
+public class LinkedList {
+    Node head;
+    Node tail;
+    int length;
+
+    class Node{
+        int value;
+        int next;
+    }
+
+    void add(int value){
+
+    };
+}
